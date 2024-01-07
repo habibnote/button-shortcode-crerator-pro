f6281afbf9bcde3afd5f0a31c7e8789c565a52ad
Habibur Rahman <habibblog97@gmail.com>
2024-01-07 12:06:51 +0600

    all text domain added

--- app/ButtonSetting.php
+++ app/ButtonSetting.php
@@ -50,29 +50,29 @@ class ButtonSetting {
             <div class="bsc-single-row">
 
                 <div class="bsc-column">
-                    <label for="bsc-item-type">Item Type</label>
+                    <label for="bsc-item-type"><?php esc_html_e( 'Item Type', 'bsc' ); ?></label>
                     <select name="bsc-item-type" id="bsc-item-type">
-                        <option value="link">Link</option>
+                        <option value="link"><?php esc_html_e( 'Link', 'bsc' ); ?></option>
                     </select>
                 </div>
 
                 <div class="bsc-column">
-                    <label for="bsc-btn-link">Link</label>
+                    <label for="bsc-btn-link"><?php esc_html_e( 'Link', 'bsc' ); ?></label>
                     <input type="text" name="bsc-btn-link" id="bsc-btn-link" value="<?php echo $bsc_btn_style_setting['bsc_btn_link'] ?? '' ?>">
                 </div>
                 <div class="bsc-column">
-                    <label for="bsc-new-tab">Open a New Tab</label>
+                    <label for="bsc-new-tab"><?php esc_html_e( 'Open a New Tab', 'bsc' ); ?></label>
                     <input type="checkbox" name="bsc-new-tab" id="bsc-new-tab">
                 </div>
             </div>
 
             <div class="bsc-single-row">
                 <div class="bsc-column">
-                    <label for="bsc-btn-class">Class</label>
+                    <label for="bsc-btn-class"><?php esc_html_e( 'Class', 'bsc' ); ?></label>
                     <input type="text" name="bsc-btn-class" id="bsc-btn-class" value="<?php echo $bsc_btn_style_setting['bsc_btn_class'] ?? '' ?>">
                 </div>
                 <div class="bsc-column">
-                    <label for="bsc-btn-id">ID</label>
+                    <label for="bsc-btn-id"><?php esc_html_e( 'ID', 'bsc' ); ?></label>
                     <input type="text" name="bsc-btn-id" id="bsc-btn-id" value="<?php echo $bsc_btn_style_setting['bsc_btn_id'] ?? '' ?>">
                 </div>
             </div>
@@ -87,85 +87,85 @@ class ButtonSetting {
         ?>
             <div class="bsc-single-row">
                 <div class="bsc-column">
-                    <label for="bsc-btn-width">Width(px)</label>
+                    <label for="bsc-btn-width"><?php esc_html_e( 'Width(px)', 'bsc' ); ?></label>
                     <input type="text" name="bsc-btn-width" id="bsc-btn-width" value="<?php echo $bsc_btn_style_setting['bsc_btn_width'] ?? 100 ?>">
                 </div>
                 <div class="bsc-column">
-                    <label for="bsc-btn-height">Height(px)</label>
+                    <label for="bsc-btn-height"><?php esc_html_e( 'Height(px)', 'bsc' ); ?></label>
                     <input type="text" name="bsc-btn-height" id="bsc-btn-height" value="<?php echo $bsc_btn_style_setting['bsc_btn_height'] ?? 50 ?>">
                 </div>
                 <div class="bsc-column">
-                    <label for="bsc-btn-z_index">Z-index</label>
+                    <label for="bsc-btn-z_index"><?php esc_html_e( 'Z-index', 'bsc' ); ?></label>
                     <input type="text" name="bsc-btn-z_index" id="bsc-btn-z_index" value="<?php echo $bsc_btn_style_setting['bsc_btn_z_index'] ?? 1000 ?>">
                 </div>
             </div>
             <div class="bsc-single-row">
                 <div class="bsc-column">
-                    <label>Color</label>
+                    <label><?php esc_html_e( 'Color', 'bsc' ); ?></label>
                     <div class="color-picker-single">
                         <input type="text" name="bsc-btn-color" id="bsc-btn-color" value="<?php echo $bsc_btn_style_setting['bsc_btn_color'] ?? '#000000' ?>">
-                        <label class="bsc-label">Select Color</label>
+                        <label class="bsc-label"> <?php esc_html_e( 'Select Color', 'bsc' ); ?></label>
                     </div>
                 </div>
                 <div class="bsc-column">
-                    <label>Background</label>
+                    <label><?php esc_html_e( 'Background', 'bsc' ); ?></label>
                     <div class="color-picker-single">
                         <input type="text" name="bsc-btn-bg-color" id="bsc-btn-bg-color" value="<?php echo $bsc_btn_style_setting['bsc_btn_bg_color'] ?? '#1e73be' ?>">
-                        <label class="bsc-label">Select Color</label>
+                        <label class="bsc-label"><?php esc_html_e( 'Select Color', 'bsc' ); ?></label>
                     </div>
                 </div>
             </div>
             <div class="bsc-single-row">
                 <div class="bsc-column">
-                    <label>Hover Color</label>
+                    <label><?php esc_html_e( 'Hover Color', 'bsc' ); ?></label>
                     <div class="color-picker-single">
                         <input type="text" name="bsc-btn-hover-color" id="bsc-btn-hover-color" value="<?php echo $bsc_btn_style_setting['bsc_btn_hover_color'] ?? '#000000' ?>">
-                        <label class="bsc-label">Select Color</label>
+                        <label class="bsc-label"><?php esc_html_e( 'Select Color', 'bsc' ); ?></label>
                     </div>
                 </div>
                 <div class="bsc-column">
-                    <label>Hover Background</label>
+                    <label><?php esc_html_e( 'Hover Background', 'bsc' ); ?></label>
                     <div class="color-picker-single">
                         <input type="text" name="bsc-btn-hover-bg-color" id="bsc-btn-hover-bg-color" value="<?php echo $bsc_btn_style_setting['bsc_btn_hover_bg_color'] ?? '#1e73be' ?>">
-                        <label class="bsc-label">Select Color</label>
+                        <label class="bsc-label"><?php esc_html_e( 'Select Color', 'bsc' ); ?></label>
                     </div>
                 </div>
                 <div class="bsc-column">
-                    <label for="bsc-hover-effect">Hover Effect</label>
+                    <label for="bsc-hover-effect"><?php esc_html_e( 'Hover Effect', 'bsc' ); ?></label>
                     <select name="bsc-hover-effect" id="bsc-hover-effect">
-                        <option value="none">none</option>
+                        <option value="none"><?php esc_html_e( 'none', 'bsc' ); ?></option>
                     </select>
                 </div>
             </div>
             <div class="bsc-single-row">
                 <div class="bsc-column">
-                    <label for="bsc-border-radius">Radius(px)</label>
+                    <label for="bsc-border-radius"><?php esc_html_e( 'Radius(px)', 'bsc' ); ?></label>
                     <input type="text" name="bsc-border-radius" id="bsc-border-radius" value="<?php echo $bsc_btn_style_setting['bsc_border_radius'] ?? 10 ?>">
                 </div>
             </div>
             <div class="bsc-single-row">
                 <div class="bsc-column">
-                    <label for="bsc-shadow">Shadow</label>
+                    <label for="bsc-shadow"><?php esc_html_e( 'Shadow', 'bsc' ); ?></label>
                     <select name="bsc-shadow" id="bsc-shadow">
-                        <option value="none">none</option>
+                        <option value="none"><?php esc_html_e( 'none', 'bsc' ); ?></option>
                     </select>
                 </div>
             </div>
             <div class="bsc-single-row">
                 <div class="bsc-column">
-                    <label for="bsc-btn-font-size">Font Size</label>
+                    <label for="bsc-btn-font-size"><?php esc_html_e( 'Font Size', 'bsc' ); ?></label>
                     <input type="text" name="bsc-btn-font-size" id="bsc-btn-font-size" value="<?php echo $bsc_btn_style_setting['bsc_btn_font_size'] ?? 16 ?>">
                 </div>
                 <div class="bsc-column">
-                    <label for="bsc-font-weight">Font Weight</label>
+                    <label for="bsc-font-weight"><?php esc_html_e( 'Font Weight', 'bsc' ); ?></label>
                     <select name="bsc-font-weight" id="bsc-font-weight">
-                        <option value="none">none</option>
+                        <option value="none"><?php esc_html_e( 'none', 'bsc' ); ?></option>
                     </select>
                 </div>
                 <div class="bsc-column">
-                    <label for="bsc-font-style">Font Style</label>
+                    <label for="bsc-font-style"><?php esc_html_e( 'Font Style', 'bsc' ); ?></label>
                     <select name="bsc-font-style" id="bsc-font-style">
-                        <option value="none">none</option>
+                        <option value="none"><?php esc_html_e( 'none', 'bsc' ); ?></option>
                     </select>
                 </div>
             </div>
